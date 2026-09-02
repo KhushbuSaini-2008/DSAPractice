72619b488b3c5142ca5767de2139407720f0f112
Khushbu_Saini <khushbusaini24.set@modyuniversity.ac.in>
2026-09-02 22:01:08 +0530

    Time: 4 ms (23.27%), Space: 112.3 MB (88.05%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+      int start=0;
+      int deficit=0;
+      int balance=0;
+      for(int i=0;i<gas.size();i++){
+        if(balance+gas[i]<cost[i]){
+            deficit+=balance+gas[i]-cost[i];
+            start=i+1;
+            balance=0;
+        }
+        else{
+            balance=balance+gas[i]-cost[i];
+        }
+      }  
+      if(balance+deficit>=0){
+        return start;
+      }
+      else{
+      return -1;
+      }
+    }
+};
