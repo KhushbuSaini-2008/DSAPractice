1a58c2693ae8a91a646f9f5037c6793ba2cc37d1
Khushbu_Saini <khushbusaini24.set@modyuniversity.ac.in>
2026-09-20 20:19:15 +0530

    Time: 0 ms (100%), Space: 9.9 MB (15.78%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int ans=0;
+        
+       
+     for(int i=0;i<s.length();i++){
+        int value=26-(s[i]-'a');
+        int index=i+1;
+        ans+=index*value;
+        
+
+
+     }
+return ans;
+
+            
+      
+    }
+};
